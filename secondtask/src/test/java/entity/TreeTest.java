0765a3d5cd8216e56c3f39702f6797b204970dfe
EltayGasimovJT <eltay.gasimov.jazzteam@gmail.com>
2021-07-08 21:33:49 +0300

    added test for second task plugins for jenkins

--- secondtask/src/test/java/entity/TreeTest.java
+++ secondtask/src/test/java/entity/TreeTest.java
@@ -1,9 +1,11 @@
 package entity;
 
+import creator.TreeCreator;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TreeTest {
+    private static final int[] EXPECTED_ARRAY = {1, 2, 3, 4, 5, 6};
 
     @Test
     public void getCountOfLeafTest() {
@@ -15,6 +17,25 @@ public class TreeTest {
         tree.add(11);
 
         int actual = tree.getRoot().getCountOfLeaf();
+
+        Assert.assertEquals(expected, actual, 0.001);
+    }
+
+    @Test
+    public void creatorAndCountOfLeafTest() {
+        Tree expectedTree = new Tree();
+
+        expectedTree.add(1);
+        expectedTree.add(2);
+        expectedTree.add(3);
+        expectedTree.add(4);
+        expectedTree.add(5);
+        expectedTree.add(6);
+
+        Tree actualTree = TreeCreator.createTree(EXPECTED_ARRAY);
+
+        int expected = expectedTree.getRoot().getCountOfLeaf();
+        int actual = actualTree.getRoot().getCountOfLeaf();
 
         Assert.assertEquals(expected, actual, 0.001);
     }
