86d59679bc689040cf057435fba0e7ebeab77d00
EltayGasimovJT <eltay.gasimov.jazzteam@gmail.com>
2021-07-15 05:46:10 +0300

    fixed some remarks from second task

--- fourthTask/src/test/java/service/TriangleServiceTest.java
+++ fourthTask/src/test/java/service/TriangleServiceTest.java
@@ -3,37 +3,78 @@ package service;
 import entity.Point;
 import entity.Triangle;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
 public class TriangleServiceTest {
-    private static final Triangle TEST_TRIANGLE = new Triangle(
-            new Point(1, 1),
-            new Point(1, 8),
-            new Point(7, 1)
-    );
+    private static Stream<Arguments> isTriangleTestData() {
+        Triangle firstTriangle = new Triangle(
+                new Point(1, 1),
+                new Point(1, 8),
+                new Point(7, 1)
+        );
 
-    private static Stream<Arguments> provideStringsForIsBlank() {
+        Triangle secondTriangle = new Triangle(
+                new Point(0, 0),
+                new Point(0, 0),
+                new Point(0, 0)
+        );
+
+        Triangle thirdTriangle = new Triangle(
+                new Point(-5, -1),
+                new Point(-2, 1),
+                new Point(4, -2)
+        );
+
         return Stream.of(
-                Arguments.of(null, true),
-                Arguments.of("", true),
-                Arguments.of("  ", true),
-                Arguments.of("not blank", false)
+                Arguments.of(firstTriangle, true),
+                Arguments.of(secondTriangle, false),
+                Arguments.of(thirdTriangle, true)
         );
     }
 
-    @Test
-    public void isTriangleTest() {
-        boolean actual = TriangleService.isTriangle(TEST_TRIANGLE);
-        Assert.assertEquals(actual, true);
+    private static Stream<Arguments> trianglesToTest() {
+        Triangle firstTriangle = new Triangle(
+                new Point(1, 1),
+                new Point(1, 8),
+                new Point(7, 1)
+        );
+
+        Triangle secondTriangle = new Triangle(
+                new Point(0, 0),
+                new Point(0, 0),
+                new Point(0, 0)
+        );
+
+        Triangle thirdTriangle = new Triangle(
+                new Point(-5, -1),
+                new Point(-2, 1),
+                new Point(4, -2)
+        );
+
+        return Stream.of(
+                Arguments.of(firstTriangle, new Point(1, 4), true),
+                Arguments.of(secondTriangle, new Point(0, 0), false),
+                Arguments.of(thirdTriangle, new Point(0, 0), true)
+        );
     }
 
-    @Test
-    public void checkIfPointBelongsToTriangleTest() {
-        Point point = new Point(1,4);
-        boolean actual = TriangleService.isBelongsToTriangle(point, TEST_TRIANGLE);
+
+    @ParameterizedTest
+    @MethodSource("isTriangleTestData")
+    public void isTriangleTest(Triangle triangle, boolean expected) {
+        boolean actual = TriangleService.isTriangle(triangle);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("trianglesToTest")
+    public void checkIfPointBelongsToTriangleTest(Triangle triangle, Point point) {
+
+        boolean actual = TriangleService.isBelongsToTriangle(point, triangle);
 
         Assert.assertEquals(actual, true);
     }
