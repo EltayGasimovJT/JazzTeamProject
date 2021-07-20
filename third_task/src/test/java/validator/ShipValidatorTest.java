35c4cf605da516ceb70a2681250783733c27a7f0
EltayGasimovJT <eltay.gasimov.jazzteam@gmail.com>
2021-07-20 13:41:39 +0300

    fixed some remarks in the third task

--- third_task/src/test/java/validator/ShipValidatorTest.java
+++ third_task/src/test/java/validator/ShipValidatorTest.java
@@ -2,16 +2,32 @@ package validator;
 
 import entity.Port;
 import entity.Ship;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ShipValidatorTest {
+    private static Stream<Arguments> wrongShipsDataToTest() {
+        return Stream.of(
+                Arguments.of(new Ship("ship",
+                        -2, 52, 65, new Port())),
+                Arguments.of(new Ship("ship",
+                        2, 52, -65, new Port())),
+                Arguments.of(new Ship("ship",
+                        2, -52, 65, new Port())),
+                Arguments.of(new Ship("ship",
+                        -2, -52, -65, new Port())
+                ));
+    }
 
-    @Test
-    void validateShipParametersNotNegative() {
+    @ParameterizedTest
+    @MethodSource("wrongShipsDataToTest")
+    void validateShipParametersNotNegative(Ship ship) {
         assertThrows(IllegalArgumentException.class,
-                () -> ShipValidator.validateShipParametersNotNegative(new Ship("ship",
-                        -2, -52, -65, new Port())));
+                () -> ShipValidator.validateShipParametersNotNegative(ship));
     }
 }
