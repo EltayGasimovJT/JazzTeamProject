6b2b8041de3530ed6486e69006d5a14330e7f8d1
EltayGasimovJT <eltay.gasimov.jazzteam@gmail.com>
2021-07-28 17:44:53 +0300

    added some update for the main project, version 1.8

--- mainproject/src/test/java/service/OrderProcessingPointServiceTest.java
+++ mainproject/src/test/java/service/OrderProcessingPointServiceTest.java
@@ -4,39 +4,112 @@ import entity.OrderProcessingPoint;
 import entity.Warehouse;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import service.impl.OrderProcessingPointServiceImpl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
 
 class OrderProcessingPointServiceTest {
     private final OrderProcessingPointService orderProcessingPointService = new OrderProcessingPointServiceImpl();
 
-    @Test
-    void addOrderProcessingPoint() {
-        OrderProcessingPoint orderProcessingPoint = new OrderProcessingPoint();
-        orderProcessingPoint.setId(1);
-        orderProcessingPoint.setLocation("Minsk");
-        orderProcessingPoint.setWarehouse(new Warehouse());
+    private static Stream<Arguments> testOrderProcessingPoints() {
+        OrderProcessingPoint orderProcessingPoint1 = new OrderProcessingPoint();
+        orderProcessingPoint1.setId(1);
+        orderProcessingPoint1.setLocation("Minsk");
+        orderProcessingPoint1.setWarehouse(new Warehouse());
 
+        OrderProcessingPoint orderProcessingPoint2 = new OrderProcessingPoint();
+        orderProcessingPoint2.setId(2);
+        orderProcessingPoint2.setLocation("Moskov");
+        orderProcessingPoint2.setWarehouse(new Warehouse());
+
+        OrderProcessingPoint orderProcessingPoint3 = new OrderProcessingPoint();
+        orderProcessingPoint3.setId(3);
+        orderProcessingPoint3.setLocation("Grodno");
+        orderProcessingPoint3.setWarehouse(new Warehouse());
+
+        return Stream.of(
+                Arguments.of(orderProcessingPoint1, "Minsk"),
+                Arguments.of(orderProcessingPoint2, "Moskov"),
+                Arguments.of(orderProcessingPoint3, "Grodno")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testOrderProcessingPoints")
+    void addOrderProcessingPoint(OrderProcessingPoint orderProcessingPoint, String expectedLocation) {
         orderProcessingPointService.addOrderProcessingPoint(orderProcessingPoint);
 
         String actualLocation = orderProcessingPointService.getOrderProcessingPoint(orderProcessingPoint.getId()).getLocation();
-        Assert.assertEquals("Minsk", actualLocation);
+        Assert.assertEquals(expectedLocation, actualLocation);
     }
 
     @Test
     void deleteOrderProcessingPoint() {
+        OrderProcessingPoint orderProcessingPoint1 = new OrderProcessingPoint();
+        orderProcessingPoint1.setId(1);
+
+        OrderProcessingPoint orderProcessingPoint2 = new OrderProcessingPoint();
+        orderProcessingPoint2.setId(2);
+
+        OrderProcessingPoint orderProcessingPoint3 = new OrderProcessingPoint();
+        orderProcessingPoint3.setId(3);
+
+        orderProcessingPointService.addOrderProcessingPoint(orderProcessingPoint1);
+        orderProcessingPointService.addOrderProcessingPoint(orderProcessingPoint2);
+        orderProcessingPointService.addOrderProcessingPoint(orderProcessingPoint3);
+
+        orderProcessingPointService.deleteOrderProcessingPoint(orderProcessingPoint3);
+
+        Assert.assertEquals(2, orderProcessingPointService.findAllOrderProcessingPoints().size());
     }
 
     @Test
     void findAllOrderProcessingPoints() {
+        OrderProcessingPoint orderProcessingPoint1 = new OrderProcessingPoint();
+        orderProcessingPoint1.setId(1);
+
+        OrderProcessingPoint orderProcessingPoint2 = new OrderProcessingPoint();
+        orderProcessingPoint2.setId(2);
+
+        OrderProcessingPoint orderProcessingPoint3 = new OrderProcessingPoint();
+        orderProcessingPoint3.setId(3);
+
+        orderProcessingPointService.addOrderProcessingPoint(orderProcessingPoint1);
+        orderProcessingPointService.addOrderProcessingPoint(orderProcessingPoint2);
+        orderProcessingPointService.addOrderProcessingPoint(orderProcessingPoint3);
+
+        Assert.assertEquals(3, orderProcessingPointService.findAllOrderProcessingPoints().size());
     }
 
     @Test
     void getOrderProcessingPoint() {
+        OrderProcessingPoint orderProcessingPoint1 = new OrderProcessingPoint();
+        orderProcessingPoint1.setId(1);
+        orderProcessingPoint1.setLocation("Minsk");
+
+        OrderProcessingPoint orderProcessingPoint2 = new OrderProcessingPoint();
+        orderProcessingPoint2.setId(2);
+        orderProcessingPoint2.setLocation("Moskov");
+
+        orderProcessingPointService.addOrderProcessingPoint(orderProcessingPoint1);
+        orderProcessingPointService.addOrderProcessingPoint(orderProcessingPoint2);
+
+
+        OrderProcessingPoint orderProcessingPoint = orderProcessingPointService.getOrderProcessingPoint(2);
+
+        Assert.assertEquals("Moskov", orderProcessingPoint.getLocation());
     }
 
     @Test
     void update() {
+        OrderProcessingPoint orderProcessingPoint = new OrderProcessingPoint();
+        orderProcessingPoint.setId(1);
+        orderProcessingPoint.setLocation("Minsk");
+
+
     }
 }
--- mainproject/src/test/java/service/OrderServiceTest.java
+++ mainproject/src/test/java/service/OrderServiceTest.java
@@ -1,7 +1,56 @@
 package service;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceTest {
 
+    @Test
+    void updateOrderCurrentLocation() {
+    }
+
+    @Test
+    void updateOrderHistory() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByRecipient() {
+    }
+
+    @Test
+    void findBySender() {
+    }
+
+    @Test
+    void getCurrentOrderLocation() {
+    }
+
+    @Test
+    void send() {
+    }
+
+    @Test
+    void accept() {
+    }
+
+    @Test
+    void getState() {
+    }
+
+    @Test
+    void compareOrders() {
+    }
+
+    @Test
+    void isFinalWarehouse() {
+    }
 }
