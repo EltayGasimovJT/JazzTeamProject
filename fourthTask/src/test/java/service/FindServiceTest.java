0e0de0fb333545e8aa6ae49090e9b91ecb7e506a
EltayGasimovJT <eltay.gasimov.jazzteam@gmail.com>
2021-07-28 14:33:25 +0300

    added some solution for the project

--- fourthTask/src/test/java/service/FindServiceTest.java
+++ fourthTask/src/test/java/service/FindServiceTest.java
@@ -10,17 +10,17 @@ import service.impl.FindTriangleParametersServiceImpl;
 import java.util.stream.Stream;
 
 public class FindServiceTest {
-    private static final Point[] TEST_POINTS = new Point[]{
-            new Point(1, 1),
-            new Point(1, 8)
-    };
+    private static Stream<Arguments> pointsToTest() {
+        return Stream.of(
+                Arguments.of(new Point(1, 1), new Point(1, 8)));
+    }
 
     @ParameterizedTest
     @MethodSource("pointsToTest")
-    public void findSide() {
+    public void findSide(Point point1, Point point2) {
         double expected = 7;
         FindTriangleParametersService findService = new FindTriangleParametersServiceImpl();
-        double actual = findService.findSide(TEST_POINTS[0], TEST_POINTS[1]);
+        double actual = findService.findSide(point1, point2);
 
         Assert.assertEquals(expected, actual, 0.001);
     }
