1fff7c920531a07a65b5493a3c72d0eeb7ad3d17
EltayGasimovJT <eltay.gasimov.jazzteam@gmail.com>
2021-08-04 11:17:55 +0300

    added some update for the main project, version 1.22

--- delivery_agency_servlet/src/main/java/repository/enums/CreateActionEnum.java
+++ delivery_agency_servlet/src/main/java/repository/enums/CreateActionEnum.java
@@ -3,7 +3,7 @@ package repository.enums;
 public enum CreateActionEnum {
     CREATE_CLIENT_TABLE("CREATE TABLE clients\n" +
             "(\n" +
-            "    id            INT PRIMARY KEY AUTO_INCREMENT,\n" +
+            "    id       id bigint(11) NOT NULL primary key,\n" +
             "    name      VARCHAR(60) NOT NULL,\n" +
             "    surname      VARCHAR(60) NOT NULL,\n" +
             "    passportID     VARCHAR(60) NOT NULL,\n" +
