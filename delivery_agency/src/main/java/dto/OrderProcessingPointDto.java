6642289946dd6c026a5a0e79b11f884300da04bb
User <gasimov20177@mail.ru>
2021-08-13 13:46:59 +0300

    added some solution for the delivery_agency version 1.46

--- delivery_agency/src/main/java/dto/OrderProcessingPointDto.java
+++ delivery_agency/src/main/java/dto/OrderProcessingPointDto.java
@@ -1,11 +1,9 @@
 package dto;
 
-import entity.Warehouse;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class OrderProcessingPointDto extends AbstractBuildingDto{
     private Long id;
-    private Warehouse warehouse;
+    private WarehouseDto warehouse;
 }
