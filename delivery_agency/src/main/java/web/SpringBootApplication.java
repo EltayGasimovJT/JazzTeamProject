3dbe73c12549a1fdad73b1a64e93d68184cb02ce
User <gasimov20177@mail.ru>
2021-08-19 16:42:58 +0300

    added some solution for the delivery_agency version 1.70

--- delivery_agency/src/main/java/web/SpringBootApplication.java
+++ delivery_agency/src/main/java/web/SpringBootApplication.java
@@ -1,10 +1,9 @@
 package web;
 
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
-@Slf4j
 public class SpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplication.class, args);
