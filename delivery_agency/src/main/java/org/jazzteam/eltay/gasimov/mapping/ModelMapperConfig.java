60762e71e799df9633fe58cf3e4cd5f3cab63556
EltayGasimovJT <eltay.gasimov.jazzteam@gmail.com>
2021-08-20 09:07:05 +0300

    added some update for the main project, version 1.82

--- delivery_agency/src/main/java/org/jazzteam/eltay/gasimov/mapping/ModelMapperConfig.java
+++ delivery_agency/src/main/java/org/jazzteam/eltay/gasimov/mapping/ModelMapperConfig.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ModelMapperConfig {
+
     @Bean(value = "customModelMapper")
     public ModelMapper modelMapper(){
         return new ModelMapper();
