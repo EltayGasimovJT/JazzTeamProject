e93831b0f8e7bb7de367179a0c79e437d6e26b8e
= <gasimov20177@mail.ru>
2021-08-20 18:16:25 +0300

    added some solution for the delivery_agency version 1.70

--- delivery_agency/src/main/java/org/jazzteam/eltay/gasimov/SpringBootApplication.java
+++ delivery_agency/src/main/java/org/jazzteam/eltay/gasimov/SpringBootApplication.java
@@ -1,8 +1,9 @@
 package org.jazzteam.eltay.gasimov;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@org.springframework.boot.autoconfigure.SpringBootApplication
+@org.springframework.boot.autoconfigure.SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApplication.class, args);
