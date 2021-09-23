ca54148a1e458cda8d3c22d6a0d1fc649988dbc6
= <gasimov20177@mail.ru>
2021-09-23 11:47:31 +0300

    Sort was stayed only by date

--- delivery_agency/src/main/resources/static/js/clientOrders.js
+++ delivery_agency/src/main/resources/static/js/clientOrders.js
@@ -157,9 +157,9 @@ const sortTable = function (index, table, type) {
             }
             case 'date': {
                 if (moment(rowDataA).isBefore(rowDataB)) {
-                    return -1;
-                } else if (moment(rowDataA).isAfter(rowDataB)) {
                     return 1;
+                } else if (moment(rowDataA).isAfter(rowDataB)) {
+                    return -1;
                 } else return 0;
                 break;
             }
